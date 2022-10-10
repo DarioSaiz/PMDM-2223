2a889fd9ca7d135f3a942264e07e0f541bfe06b5
Miguel Ángel desde clase <miguelsegoviafreeman@gmail.com>
2022-10-10 18:00:10 +0200

    10/10/2022

---
+++ app/src/main/java/com/example/pmdm_2223/Manejadora.java
@@ -0,0 +1,11 @@
+package com.example.pmdm_2223;
+
+import android.view.KeyEvent;
+import android.widget.TextView;
+
+public class Manejadora implements TextView.OnEditorActionListener {
+    @Override
+    public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+        return false;
+    }
+}
