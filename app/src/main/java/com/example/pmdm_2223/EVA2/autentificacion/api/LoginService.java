c1845f8fb167ff00bb771c4cc70cda430ff258fe
Miguel Ángel desde clase <miguelsegoviafreeman@gmail.com>
2023-02-07 17:54:33 +0100

    gradle

---
+++ app/src/main/java/com/example/pmdm_2223/EVA2/autentificacion/api/LoginService.java
@@ -0,0 +1,8 @@
+package com.example.pmdm_2223.EVA2.autentificacion.api;
+
+public interface LoginService {
+
+    //http://192.168.20.200:8000/polls/api-token-auth/
+
+    //http://192.168.20.117:8000/polls/api/question/
+}
---
+++ app/src/main/java/com/example/pmdm_2223/EVA2/autentificacion/api/TokenRepository.java
@@ -0,0 +1,4 @@
+package com.example.pmdm_2223.EVA2.autentificacion.api;
+
+public class TokenRepository {
+}
